e7743e9aca4bd655a754c289291031a18f9a3e55
Julio <julio.vallsmartinez@amaris.com>
2018-04-24 09:54:17 +0200

    Refactor pages
    PrecioPage
    DatosBasicosTomadorPage

--- src/main/java/com/project/pages/DatosBasicosTomadorPage.java
+++ src/main/java/com/project/pages/DatosBasicosTomadorPage.java
@@ -2,74 +2,77 @@ package com.project.pages;
 
 import org.junit.Assert;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.mutuaPropietarios.WebdriverContext.BrowserContext;
-import com.mutuaPropietarios.WebdriverContext.Helpers.DniGeneratorHelper;
-import com.mutuaPropietarios.WebdriverContext.Helpers.WebElementHelper;
-import com.mutuaPropietarios.testCasesData.context.ProjectConstants;
-import com.mutuaPropietarios.testCasesData.context.TestCaseData;
+import com.automation.model.helpers.DniGeneratorHelper;
+import com.automation.model.testing.TestDataManager;
+import com.automation.model.webdriver.DriverHelper;
+//import com.mutuaPropietarios.WebdriverContext.BrowserContext;
+//import com.mutuaPropietarios.WebdriverContext.Helpers.DniGeneratorHelper;
+//import com.mutuaPropietarios.WebdriverContext.Helpers.WebElementHelper;
+//import com.mutuaPropietarios.testCasesData.context.ProjectConstants;
+//import com.mutuaPropietarios.testCasesData.context.TestCaseData;
 import com.paulhammant.ngwebdriver.NgWebDriver;
+import com.project.ProjectConstants;
 
 public class DatosBasicosTomadorPage
 {
-	final static Logger logger = LoggerFactory.getLogger(DatosBasicosTomadorPage.class);
-	BrowserContext browserContext;
-	protected NgWebDriver ngWebDriver;
-	private WebElementHelper wh;
-	TestCaseData tData;
+	private String testId;
+	private TestDataManager tCData;
+	private DriverHelper webDriver;
+	final static Logger logger = LoggerFactory.getLogger(PageObject.class);
+
 
 	// region webelements
-	@FindBy(name = "cuerpo")
-	private WebElement cuerpoFrame;
+	//@FindBy(name = "cuerpo")
+	private By cuerpoFrame = By.name("cuerpo");
 
-	@FindBy(id = "tipoDocumento")
-	private WebElement cmbTipoDocumento;
+	//@FindBy(id = "tipoDocumento")
+	private By cmbTipoDocumento = By.cssSelector("tipoDocumento");
 
-	@FindBy(id = "numeroDocumento")
-	private WebElement txtNumeroDocumento;
+	//@FindBy(id = "numeroDocumento")
+	private By txtNumeroDocumento = By.cssSelector("numeroDocumento");
 
-	@FindBy(xpath = ".//*[text()='Validar cliente']")
-	private WebElement btnValidarCliente;
+	//@FindBy(xpath = ".//*[text()='Validar cliente']")
+	private By btnValidarCliente = By.xpath(".//*[text()='Validar cliente']");
 
-	@FindBy(xpath = ".//*[@name='nombreTomador']")
-	private WebElement txtNombreTomador;
+	//@FindBy(xpath = ".//*[@name='nombreTomador']")
+	private By txtNombreTomador = By.xpath(".//*[@name='nombreTomador']");
 
-	@FindBy(xpath = ".//*[@name='apellido1Tomador']")
-	private WebElement txtPrimerApellidoTomador;
+	//@FindBy(xpath = ".//*[@name='apellido1Tomador']")
+	private By txtPrimerApellidoTomador = By.xpath(".//*[@name='apellido1Tomador']");
 
-	@FindBy(xpath = ".//*[@name='apellido2Tomador']")
-	private WebElement txtSegundoApellidoTomador;
+	//@FindBy(xpath = ".//*[@name='apellido2Tomador']")
+	private By txtSegundoApellidoTomador = By.xpath(".//*[@name='apellido2Tomador']");
 
-	@FindBy(xpath = ".//*[text()='Continuar']")
-	private WebElement btnContinuar;
+	//@FindBy(xpath = ".//*[text()='Continuar']")
+	private By btnContinuar = By.xpath(".//*[text()='Continuar']");
 
-	@FindBy(xpath = ".//*[text()='Aceptar']")
-	private WebElement btnAceptar;
+	//@FindBy(xpath = ".//*[text()='Aceptar']")
+	private By btnAceptar = By.xpath(".//*[text()='Aceptar']");
 
-	@FindBy(xpath = ".//*[@ng-model='tomador.cifTramite']")
-	private WebElement chkCIFEnTramite;
+	//@FindBy(xpath = ".//*[@ng-model='tomador.cifTramite']")
+	private By chkCIFEnTramite = By.xpath(".//*[@ng-model='tomador.cifTramite']");
 
-	@FindBy(name = "razonSocial")
-	private WebElement txtRazonSocial;
+	//@FindBy(name = "razonSocial")
+	private By txtRazonSocial = By.name("razonSocial");
 
-	@FindBy(name = "duplicadoSelect")
-	private WebElement rdnClienteExistente;
+	//@FindBy(name = "duplicadoSelect")
+	private By rdnClienteExistente = By.name("duplicadoSelect");
 
-	@FindBy(xpath = ".//*[text()='Seleccionar cliente']")
-	private WebElement btnSeleccionarCliente;
+	//@FindBy(xpath = ".//*[text()='Seleccionar cliente']")
+	private By btnSeleccionarCliente = By.xpath(".//*[text()='Seleccionar cliente']");
 
 	// endregion
 
-	public DatosBasicosTomadorPage(BrowserContext browserContext)
-	{
-		this.browserContext = browserContext;
-		this.wh = browserContext.webElementHelper;
-		this.tData = browserContext.getTestCaseData();
-		PageFactory.initElements(browserContext.getWebDriver(), this);
+	public DatosBasicosTomadorPage(DriverHelper driver, TestDataManager data) {
+		this.tCData = data;
+		this.webDriver = driver;
+		this.testId = webDriver.getId() == null ? "" : webDriver.getId();
 	}
 
 	public void ExecuteActionsInPageTomadorYAseguradoPage(
@@ -83,29 +86,31 @@ public class DatosBasicosTomadorPage
 	public void clickOnContinuar()
 	{
 		logger.debug("BEGIN - ClickOnContinuar");
-		this.wh.clickOnWebElementInFrame(this.btnContinuar, this.cuerpoFrame);
+		this.webDriver.clickInFrame(this.btnContinuar, this.cuerpoFrame);
 		logger.debug("END - ClickOnContinuar");
 	}
 	
 	public void fillStaticTomadorData()
 	{
-		this.wh.selectValueInDropDownInFrame(this.cmbTipoDocumento, this.cuerpoFrame, ProjectConstants.NIF);
-		this.browserContext.getTestCaseData().setTomadorDNI(DniGeneratorHelper.generaNif(null));
-		this.wh.clickOnWebElementInFrame(this.txtNumeroDocumento, this.cuerpoFrame);
-		this.wh.sendValueToWebElementInFrame(this.txtNumeroDocumento, this.cuerpoFrame, String.valueOf(this.tData.getTomadorDNI()));
+		this.webDriver.clickElementFromDropDownByTextInFrame(this.cmbTipoDocumento, this.cuerpoFrame, ProjectConstants.NIF);
+		//this.browserContext.getTestCaseData().setTomadorDNI(DniGeneratorHelper.generaNif(null));
+		this.tCData.setTestVar(testId, "tomadorDNI", DniGeneratorHelper.generaNif(null));
+		this.webDriver.clickInFrame(this.txtNumeroDocumento, this.cuerpoFrame);
+		//this.webDriver.sendValueToWebElementInFrame(this.txtNumeroDocumento, this.cuerpoFrame, String.valueOf(this.tCData.getTomadorDNI()));
+		this.webDriver.appendTextInFrame(this.txtNumeroDocumento, this.cuerpoFrame, String.valueOf(this.tCData.getTestVar(testId, "tomadorDNI")));
 		
 		// Select nombre tomador
-		this.wh.sendValueToWebElementInFrame(this.txtNombreTomador, this.cuerpoFrame, "Tomnombre");
+		this.webDriver.appendTextInFrame(this.txtNombreTomador, this.cuerpoFrame, "Tomnombre");
 		// Select primer apellido
-		this.wh.sendValueToWebElementInFrame(this.txtPrimerApellidoTomador, this.cuerpoFrame, "Tompripellido");
+		this.webDriver.appendTextInFrame(this.txtPrimerApellidoTomador, this.cuerpoFrame, "Tompripellido");
 		// Select segundo apellido
-		this.wh.sendValueToWebElementInFrame(this.txtSegundoApellidoTomador, this.cuerpoFrame, "Tomsegapellido");
+		this.webDriver.appendTextInFrame(this.txtSegundoApellidoTomador, this.cuerpoFrame, "Tomsegapellido");
 		
 		// this.wh.switchToFrame(this.cuerpoFrame);
 		// this.wh.changeFocusOfWebElement(this.txtSegundoApellidoTomador);
 		// this.wh.exitFromFrame();
-		this.wh.clickOnWebElementInFrame(this.btnValidarCliente, this.cuerpoFrame);
-		this.wh.clickOnWebElementInFrame(this.btnAceptar, this.cuerpoFrame);
+		this.webDriver.clickInFrame(this.btnValidarCliente, this.cuerpoFrame);
+		this.webDriver.clickInFrame(this.btnAceptar, this.cuerpoFrame);
 	}
 	
 	public void FillTomadorData(
@@ -125,27 +130,31 @@ public class DatosBasicosTomadorPage
 				// this.browserContext.getTestCaseData().getTomadorSegundoApellido());
 				
 				// Select documento tomador
-				this.wh.selectValueInDropDownInFrame(this.cmbTipoDocumento, this.cuerpoFrame, ProjectConstants.NIF);
-				this.browserContext.getTestCaseData().setTomadorDNI(DniGeneratorHelper.generaNif(null));
-				this.wh.clickOnWebElementInFrame(this.txtNumeroDocumento, this.cuerpoFrame);
-				this.wh.sendValueToWebElementInFrame(this.txtNumeroDocumento, this.cuerpoFrame, String.valueOf(this.tData.getTomadorDNI()));
+				this.webDriver.clickElementFromDropDownByTextInFrame(this.cmbTipoDocumento, this.cuerpoFrame, ProjectConstants.NIF);
+				this.tCData.setTestVar(testId, "tomadorDNI", DniGeneratorHelper.generaNif(null));
+				this.webDriver.clickInFrame(this.txtNumeroDocumento, this.cuerpoFrame);
+				this.webDriver.appendTextInFrame(this.txtNumeroDocumento, this.cuerpoFrame, String.valueOf(this.tCData.getTestVar(testId, "tomadorDNI")));
 				
 				// Select nombre tomador
-				this.wh.sendValueToWebElementInFrame(this.txtNombreTomador, this.cuerpoFrame, String.valueOf(this.tData.getTomadorNombre()));
+				//this.wh.sendValueToWebElementInFrame(this.txtNombreTomador, this.cuerpoFrame, String.valueOf(this.tData.getTomadorNombre()));
+				this.webDriver.appendTextInFrame(this.txtNombreTomador, this.cuerpoFrame, String.valueOf(this.tCData.getTestVar(testId, "nombre_tomador")));
+				
 				// Select primer apellido
-				this.wh.sendValueToWebElementInFrame(this.txtPrimerApellidoTomador, this.cuerpoFrame, String.valueOf(this.tData.getTomadorPrimerApellido()));
+				//this.wh.sendValueToWebElementInFrame(this.txtPrimerApellidoTomador, this.cuerpoFrame, String.valueOf(this.tData.getTomadorPrimerApellido()));
+				this.webDriver.appendTextInFrame(this.txtPrimerApellidoTomador, this.cuerpoFrame, String.valueOf(this.tCData.getTestVar(testId, "primer_apellido_tomador")));
+				
 				// Select segundo apellido
-				this.wh.sendValueToWebElementInFrame(this.txtSegundoApellidoTomador, this.cuerpoFrame,
-						String.valueOf(this.tData.getTomadorSegundoApellido()));
+				//this.wh.sendValueToWebElementInFrame(this.txtSegundoApellidoTomador, this.cuerpoFrame, String.valueOf(this.tData.getTomadorSegundoApellido()));
+				this.webDriver.appendTextInFrame(this.txtSegundoApellidoTomador, this.cuerpoFrame, String.valueOf(this.tCData.getTestVar(testId, "segundo_apellido_tomador")));
 				
 				// this.wh.switchToFrame(this.cuerpoFrame);
 				// this.wh.changeFocusOfWebElement(this.txtSegundoApellidoTomador);
 				// this.wh.exitFromFrame();
-				this.wh.clickOnWebElementInFrame(this.btnValidarCliente, this.cuerpoFrame);
-				this.wh.clickOnWebElementInFrame(this.btnAceptar, this.cuerpoFrame);
+				this.webDriver.clickInFrame(this.btnValidarCliente, this.cuerpoFrame);
+				this.webDriver.clickInFrame(this.btnAceptar, this.cuerpoFrame);
 				break;
 			case ProjectConstants.ClienteExistente:
-				this.wh.selectValueInDropDownInFrame(this.cmbTipoDocumento, this.cuerpoFrame, ProjectConstants.NIF);
+				this.webDriver.clickElementFromDropDownByTextInFrame(this.cmbTipoDocumento, this.cuerpoFrame, ProjectConstants.NIF);
 				this.browserContext.getTestCaseData().setTomadorDNI(this.browserContext.getTestCaseData().getTomadorDNI());
 				this.wh.sendValueToWebElementInFrame(this.txtNumeroDocumento, this.cuerpoFrame, this.browserContext.getTestCaseData().getTomadorDNI());
 				this.wh.clickOnWebElementInFrame(this.txtNombreTomador, this.cuerpoFrame);
--- src/main/java/com/project/pages/PrecioPage.java
+++ src/main/java/com/project/pages/PrecioPage.java
@@ -1,34 +1,37 @@
 package com.project.pages;
 
 import java.util.List;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.mutuaPropietarios.WebdriverContext.BrowserContext;
-import com.mutuaPropietarios.WebdriverContext.Helpers.WebElementHelper;
-import com.mutuaPropietarios.testCasesData.context.TestCaseData;
+import com.automation.model.testing.TestDataManager;
+import com.automation.model.webdriver.DriverHelper;
+//import com.mutuaPropietarios.WebdriverContext.BrowserContext;
+//import com.mutuaPropietarios.WebdriverContext.Helpers.WebElementHelper;
+//import com.mutuaPropietarios.testCasesData.context.TestCaseData;
 
 public class PrecioPage
 {
-	final static Logger logger = LoggerFactory.getLogger(PrecioPage.class);
-	BrowserContext browserContext;
-	private WebElementHelper wh;
-	TestCaseData tData;
+	private String testId;
+	private TestDataManager tCData;
+	private DriverHelper webDriver;
+	final static Logger logger = LoggerFactory.getLogger(PageObject.class);
+
 	
 	// region webelements
-	@FindBy(name = "cuerpo")
-	private WebElement cuerpoFrame;
+	//@FindBy(name = "cuerpo")
+	private By cuerpoFrame = By.name("cuerpo");
 	
 	// private String cssFilterSelectedCheckboxes = "input:checked[type=checkbox]";
 	// private String xPathFilterClausulaText = "/td[6]";
 	// private String xPathFilterClausulaNumber = "/td[4]";
 	
-	@FindBy(xpath = ".//*[contains(text(),'Convertir a proyecto')]")
-	private WebElement btnConvertirAProyecto;
+	//@FindBy(xpath = ".//*[contains(text(),'Convertir a proyecto')]")
+	private By btnConvertirAProyecto = By.xpath(".//*[contains(text(),'Convertir a proyecto')]");
 	
 	// checkbox of a clausula
 	// @FindBy(xpath = ".//tr[td[contains(text(),'Responsabilidad Civil y daños propios causados por
@@ -40,32 +43,30 @@ public class PrecioPage
 	// private WebElement chkSelectedCheckboxes;
 	
 	// Rows with a checkbox and a clausula
-	@FindBy(xpath = ".//tr[td[input]]")
-	private List<WebElement> rowCheckboxWithClausula;
+	//@FindBy(xpath = ".//tr[td[input]]")
+	private By rowCheckboxWithClausula = By.xpath(".//tr[td[input]]");
 	
-	@FindBy(xpath = ".//*[@class='pagination' and ancestor::*[contains(@ng-if,'control.totalPages')]]")
-	private WebElement clausulaPageSelector;
+	//@FindBy(xpath = ".//*[@class='pagination' and ancestor::*[contains(@ng-if,'control.totalPages')]]")
+	private By clausulaPageSelector = By.xpath(".//*[@class='pagination' and ancestor::*[contains(@ng-if,'control.totalPages')]]");
 	
-	@FindBy(xpath = ".//*[text()='Continuar']")
-	private WebElement btnContinuar;
+	//@FindBy(xpath = ".//*[text()='Continuar']")
+	private By btnContinuar = By.xpath(".//*[text()='Continuar']");
 	
-	@FindBy(css = "table.wideBox strong")
-	private WebElement msgAvisoSistema;
+	//@FindBy(css = "table.wideBox strong")
+	private By msgAvisoSistema = By.cssSelector("table.wideBox strong");
 
-	@FindBy(id = "logo")
-	private WebElement logoMutua;
+	//@FindBy(id = "logo")
+	private By logoMutua = By.cssSelector("#logo");
 	
-	@FindBy(id = "jt2")
-	private WebElement productoMec;
+	//@FindBy(id = "jt2")
+	private By productoMec = By.cssSelector("jt2");
 
 	// endregion
 	
-	public PrecioPage(BrowserContext browserContext)
-	{
-		this.browserContext = browserContext;
-		this.wh = browserContext.webElementHelper;
-		this.tData = browserContext.getTestCaseData();
-		PageFactory.initElements(browserContext.getWebDriver(), this);
+	public PrecioPage(DriverHelper driver, TestDataManager data) {
+		this.tCData = data;
+		this.webDriver = driver;
+		this.testId = webDriver.getId() == null ? "" : webDriver.getId();
 	}
 	
 	public void ExecuteActionsInPrecioPage()
@@ -77,9 +78,9 @@ public class PrecioPage
 	public void ClickOnConvertirAProjecto()
 	{
 		logger.debug("BEGIN - ClickOnConvertirAProjecto");
-		this.wh.clickOnWebElementInFrame(this.btnConvertirAProyecto, this.cuerpoFrame);
+		this.webDriver.clickInFrame(this.btnConvertirAProyecto, this.cuerpoFrame);
 
-		if (this.wh.webElementInFrameIsPresent(this.msgAvisoSistema, this.cuerpoFrame))
+		if (this.webDriver.isPresentInFrame(this.msgAvisoSistema, this.cuerpoFrame))
 		{
 			System.out.println("*** Excepción general al convertir a proyecto.");
 		}
