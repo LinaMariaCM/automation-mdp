ae117d7833cc3078246ba7f5d19ab30aaba27b45
Alfredo Moises Boullosa Ramones <alfredo.boullosaramones@amaris.com>
2019-08-28 14:56:22 +0200

    Change conditions order

--- src/main/java/com/amaris/automation/data/DataObject.java
+++ src/main/java/com/amaris/automation/data/DataObject.java
@@ -149,13 +149,13 @@ public class DataObject {
 	public void setValue(String rowKey, String valueKey, String value) {
 		try {
 			synchronized(data) {
-				if(rowKey != null && getRow(rowKey).containsKey(valueKey)) {
-					getRow(rowKey).replace(valueKey, value);
-				} else if(rowKey != null && getRow(rowKey) != null) {
+				if(rowKey != null && getRow(rowKey) != null) {
 					getRow(rowKey).put(valueKey, value);
 				} else if(rowKey != null && getRow(rowKey) == null) {
 					data.put(rowKey, new HashMap<String, String>());
 					getRow(rowKey).put(valueKey, value);
+				} else if(rowKey != null && getRow(rowKey).containsKey(valueKey)) {
+					getRow(rowKey).replace(valueKey, value);
 				}
 			}
 		} catch(NullPointerException e) {
