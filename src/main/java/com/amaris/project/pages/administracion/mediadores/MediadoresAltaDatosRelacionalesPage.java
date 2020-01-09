dc3c2e019f1e8d5ba962b057d207dbfe7d9cfa7c
Iryna <iryna.balan@amaris.com>
2020-01-09 13:03:41 +0100

    Create Pages

---
+++ src/main/java/com/amaris/project/pages/administracion/mediadores/MediadoresAltaDatosRelacionalesPage.java
@@ -0,0 +1,6 @@
+package com.amaris.project.pages.administracion.mediadores;
+
+import com.amaris.automation.model.testing.objects.PageObject;
+
+public class MediadoresAltaDatosRelacionalesPage extends PageObject {
+}
