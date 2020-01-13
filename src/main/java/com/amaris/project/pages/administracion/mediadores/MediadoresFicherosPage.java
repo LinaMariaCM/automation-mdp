623e28b2b80f3440004dd5ab0697bed03bbde231
Iryna <iryna.balan@amaris.com>
2020-01-13 10:58:16 +0100

    Delete Pages

--- src/main/java/com/amaris/project/pages/administracion/mediadores/MediadoresFicherosPage.java
+++
@@ -1,6 +0,0 @@
-package com.amaris.project.pages.administracion.mediadores;
-
-import com.amaris.automation.model.testing.objects.PageObject;
-
-public class MediadoresFicherosPage extends PageObject {
-}
