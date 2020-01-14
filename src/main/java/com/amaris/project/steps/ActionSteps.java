72a4c1d4cbbd4ff74f12ea4d1ac514b8c1b9ba08
mirko <mirpigar@gmail.com>
2020-01-14 16:07:40 +0100

    Update ActionSteps.java

--- src/main/java/com/amaris/project/steps/ActionSteps.java
+++ src/main/java/com/amaris/project/steps/ActionSteps.java
@@ -3089,10 +3089,9 @@ public class ActionSteps extends InteractionObject {
 
 	public void transicionar_bloques() {
 		debugBegin();
-		
-		//TODO 	comprobar por qué no está esta parte de código
-	
-		
+		new InnovaHomePage(userS).openSiniestros();
+		new GestionBuscadorSiniestrosPage(userS).buscarPorNumeroPoliza(getTestVar(Constants.NUM_POLIZA));
+		new BloqueSiniestrosPage(userS).transicionarBloqueCerrandoOrigen();
 		debugEnd();
 	}
 
