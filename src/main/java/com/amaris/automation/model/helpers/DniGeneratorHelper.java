2dea84854980cabc670f185cca589c73ed7f4088
Iryna <iryna.balan@amaris.com>
2020-01-31 09:47:47 +0100

    Update nie helper

--- src/main/java/com/amaris/automation/model/helpers/DniGeneratorHelper.java
+++ src/main/java/com/amaris/automation/model/helpers/DniGeneratorHelper.java
@@ -64,5 +64,28 @@ public class DniGeneratorHelper {
 
 		return result;
 	}
-	
+
+	public static String generateCIF(String seed) {
+		String result = null;
+		seed = seed == null ? "" : seed;
+
+		if(seed.isEmpty() || StringUtils.isNumber(seed)) {
+			String letra = "X";
+			String numeroCIF = String.valueOf(Math.random()).concat(seed);
+			numeroCIF = numeroCIF.substring(numeroCIF.length() - 5);
+
+			int valorPrimeraLetra = (int) Math.floor(Math.random() * 3);
+
+			if(valorPrimeraLetra == 1) {
+				letra = "Y";
+			} else if(valorPrimeraLetra == 2) {
+				letra = "Z";
+			}
+
+			result = letra + numeroCIF + LETRAS_ASIGNACION[
+				(Integer.parseInt(Integer.toString(valorPrimeraLetra) + numeroCIF)) % 23];
+		}
+
+		return result;
+	}
 }
