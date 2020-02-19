61b46df9ee77b8fce0501f9bdc4918cfdf41782d
Iryna <iryna.balan@amaris.com>
2020-02-19 11:30:25 +0100

    Update Constantes ficha mediador

--- src/main/java/com/amaris/project/pages/administracion/mediadores/MediadoresConstantesFicha.java
+++ src/main/java/com/amaris/project/pages/administracion/mediadores/MediadoresConstantesFicha.java
@@ -31,14 +31,17 @@ public class MediadoresConstantesFicha {
 	//ACTIVIDAD PRINCIPAL
 	public static final String ABOG = "ABOG";
 	public static final String ADFI = "ADFI";
-	public static final String API = "API";
-	public static final String GEPA = "GEPA";
 	public static final String MESE = "MESE";
 	public static final String OTRO = "OTRO";
 
 	//SEXO
 	public static final String MASC = "1";
 	public static final String FEMEN = "2";
+
+	//IDIOMA
+	public static final String CASTELLANO = "ESPA";
+	public static final String CATALAN = "CATA";
+	public static final String EUSKARA = "EUSK";
 
 	//TIPO DE DOMICILIO
 	public static final String COME = "COME";
@@ -51,26 +54,23 @@ public class MediadoresConstantesFicha {
 	public static final String VIDA = "0";
 	public static final String ACCIDENTES = "1";
 	public static final String ASISTENCIA = "10";
-	public static final String DECESOS = "11";
-	public static final String MULTI_HOGAR = "12";
-	public static final String MULTI_COMERCIO = "13";
-	public static final String MULTI_COMUNIDAD = "14";
-	public static final String MULTI_INDUSTRIA = "15";
-	public static final String OTROS_MULTI = "16";
-	public static final String OTROS_RAMOS = "17";
-	public static final String SALUD = "2";
-	public static final String TRANSPORTE_MERCANCIA = "3";
-	public static final String INCENDIOS = "4";
-	public static final String AUTOS = "5";
-	public static final String RESPONSABILIDAD_CIVIL = "6";
-	public static final String CREDITO_CAUCION = "7";
-	public static final String PERDIDAS_PECUNIARIAS = "8";
-	public static final String DEFENSA_JURIDICA = "9";
 
 	//PRODUCTO
 	public static final String COMU = "COMU";
 	public static final String HOGA = "HOGA";
 	public static final String COMERCIO = "COME";
 	public static final String ALQU = "ALQU";
+
+	//TELEFONO PRINCIPAL
+	public static final String TELEFONO1 = "911111111";
+	public static final String TELEFONO2 = "911111122";
+	public static final String TELEFONO3 = "911111133";
+
+	//EJECUTIVO COMERCIAL
+	public static final String EJECUTIVO_COMERCIAL_4000 = "4000";
+	public static final String EJECUTIVO_COMERCIAL_4001 = "4001";
+	public static final String EJECUTIVO_COMERCIAL_4003 = "4003";
+	public static final String EJECUTIVO_COMERCIAL_4007 = "4007";
+	public static final String EJECUTIVO_COMERCIAL_4010 = "4010";
 
 }
