24322154723eabea09fc91b735747a8d66efc3a1
Iryna <iryna.balan@amaris.com>
2020-03-17 17:45:13 +0100

    Update limpieza codigo

--- src/main/java/com/amaris/project/pages/administracion/mediadores/MediadoresAgendaPage.java
+++ src/main/java/com/amaris/project/pages/administracion/mediadores/MediadoresAgendaPage.java
@@ -11,7 +11,6 @@ public class MediadoresAgendaPage extends PageObject {
 
 	private By cuerpoFrame = By.cssSelector("#mainFrame");
 	private By menuFrame = By.cssSelector("#leftFrame");
-	private By topFrame = By.cssSelector("#topFrame");
 
 	//----------Nueva anotacion elementos-----------------------------------
 	private By nuevaAnotacionBtn = By.cssSelector("#cabAnotacion");
@@ -26,36 +25,17 @@ public class MediadoresAgendaPage extends PageObject {
 	private By fechaFinTareaInput = By.cssSelector("#fechaler");
 	private By grabarAnotacionBtn = By.cssSelector("body > form > table > tbody > tr > td > input");
 
-	private By plantillasBtn = By.cssSelector("#cabPlantillas");
-	private By codigoPlantillaInput = By.cssSelector("#queryPlantilla");
-	private By seleccionarPlantillaCombo = By.cssSelector("#plantilla");
-	private By buscarPlantillaBtn = By.cssSelector("#botonPlantilla");
-
-	private By documentosBtn = By.cssSelector("#cabDocumentos");
-
 	//------------------Vistas-----------------------------------
 	private By vistasBtn = By.cssSelector("#cabVistas");
-	private By vistaMediadorBtn = By.cssSelector("#MEDIADOR");
-	private By cerrarVistasElementBtn = By.cssSelector("#bloqueVistas > table > tbody > tr > td > div.sis-container-header > span > a > span");
 
 	//-----------Filtrar------------------------
 	private By filtrarBtn = By.cssSelector("#cabFiltrar");
-	private By cerrarConfFiltrosBtn = By.cssSelector("#bloqueFiltrar > table > tbody > tr > td > div.sis-container-header > span > a > span");
 
 	//-------------Leyenda------------------------------------
 	private By leyendaBtn = By.cssSelector("#cabLeyenda");
-	private By cerrarLeyendaBtn = By.cssSelector("#bloqueLeyenda > table > tbody > tr > td > div > span > a > span");
 
 	//-----------Tareas futuras------------
-	private By tareasFuturasBtn = By.cssSelector("#cabCalendario");
-	private By calendSemanaBtn = By.cssSelector("#lnkPeriodo1");
-	private By calendQuincenaBtn = By.cssSelector("#lnkPeriodo2");
-	private By calendMesBtn = By.cssSelector("#lnkPeriodo3");
-	private By calendTrimestreBtn = By.cssSelector("#lnkPeriodo4");
-	private By calendSemestreBtn = By.cssSelector("#lnkPeriodo5");
-	private By calendAnualBtn = By.cssSelector("#lnkPeriodo6");
-	private By calendBianualBtn = By.cssSelector("#lnkPeriodo7");
-	private By volverCalendarioBtn = By.cssSelector("div[id='funcionVolver'] > [id='cabAnotacion']");
+
 	private By volverAnotacionBtn = By.cssSelector("body > div.menuNav.menuNavPosAbsolute > div > ul > li > a");
 
 	//-----------Anotaciones----------
@@ -69,46 +49,6 @@ public class MediadoresAgendaPage extends PageObject {
 	}
 
 	//----------------------Métodos simples----------------------------------
-
-	public MediadoresAgendaPage clickVolverCalendario() {
-		debugBegin();
-		webDriver.clickInFrame(volverCalendarioBtn, cuerpoFrame);
-		debugEnd();
-
-		return this;
-	}
-
-	public MediadoresAgendaPage clickTareasFuturas() {
-		debugBegin();
-		webDriver.clickInFrame(tareasFuturasBtn, cuerpoFrame);
-		debugEnd();
-
-		return this;
-	}
-
-	public MediadoresAgendaPage clickCerrarLeyenda() {
-		debugBegin();
-		webDriver.clickInFrame(cerrarLeyendaBtn, cuerpoFrame);
-		debugEnd();
-
-		return this;
-	}
-
-	public MediadoresAgendaPage clickCerrarConfiguracionFiltro() {
-		debugBegin();
-		webDriver.clickInFrame(cerrarConfFiltrosBtn, cuerpoFrame);
-		debugEnd();
-
-		return this;
-	}
-
-	public MediadoresAgendaPage clickCerrarVistasElementos() {
-		debugBegin();
-		webDriver.clickInFrame(cerrarVistasElementBtn, cuerpoFrame);
-		debugEnd();
-
-		return this;
-	}
 
 	public MediadoresAgendaPage clickGrabarAnotacion() {
 		debugBegin();
